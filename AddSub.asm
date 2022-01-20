5b4af8c4553a5dd908025c550ff0089d32c32104
t2o0n321 <i915915514@icloud.com>
2022-01-20 12:29:05 +0800

    Edit AddSub.asm
    Add Irvine library to proj.

--- AddSub.asm
+++ AddSub.asm
@@ -1,16 +1,28 @@
-; AddTwo.asm - adds two 32-bit integers.
-; Chapter 3 example
+; AddTwo.asm
+; Description:
+;	Subtracts three integers using only 32-bit registers. Insert a call DumpRegs statement to display the register values.
+; HW2
 
-.386
-.model flat,stdcall
-.stack 4096
-ExitProcess proto,dwExitCode:dword
+include Irvine32.inc
+
+.data
+num_to_sub		DWORD	100h		; One WORD eqaul 16 bits that is 2 bytes
+val1			DWORD	30h			; First num to be sub
+val2			DWORD	10h			; Second num to be sub
+val3			DWORD	5h			; Last num to be sub
 
 .code
 main proc
-	mov	eax,5				
-	add	eax,6				
+	xor eax, eax					; Init EAX
+	mov	eax, num_to_sub				; Move num_to_sub into EAX
+	xor edx, edx					; Init EDX
+	mov edx, val1					; Move val1 into EDX
+	add edx, val2					; EDX += val2
+	add edx, val3					; EDX += val3
+	sub eax, edx					; EAX -= EDX
 
-	invoke ExitProcess,0
+	call DumpRegs
+
+	exit
 main endp
 end main
